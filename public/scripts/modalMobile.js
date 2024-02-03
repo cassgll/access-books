a91880afc48687f2f2784b53dd138912bcace867
Cassio Gabriel <cassgabriel@outlook.com>
2024-02-03 16:00:08 -0300

    feat: adding function on menu mobile

---
+++ public/scripts/modalMobile.js
@@ -0,0 +1,19 @@
+const buttonMobile = document.querySelector(
+	".button__mobile"
+);
+
+let nav = document.querySelector("#nav");
+
+function toggleMenu(event) {
+	if (event.type === "touchstart") event.preventDefault();
+	nav.classList.toggle("active");
+}
+
+buttonMobile.addEventListener("click", toggleMenu);
+buttonMobile.addEventListener("touchstart", toggleMenu);
+
+const menuItems = document.querySelector("#menu").children;
+for (let index = 0; index < menuItems.length; index++) {
+	let item = menuItems[index];
+	item.addEventListener("click", toggleMenu);
+}
