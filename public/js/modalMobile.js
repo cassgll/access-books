c7aaaa3851f3b187ddbbfb9c17fd8ed3c5d829d5
Cassio Gabriel <cassgabriel@outlook.com>
2024-02-28 08:19:19 -0300

    fix: fixing navigation error, style

--- public/js/modalMobile.js
+++ public/js/modalMobile.js
@@ -9,11 +9,16 @@ function toggleMenu(event) {
 	nav.classList.toggle("active");
 }
 
+function toggleMenuItems(){
+	if (event.type === "touchstart") event.preventDefault();
+	nav.classList.remove("active");
+}
+
 buttonMobile.addEventListener("click", toggleMenu);
 buttonMobile.addEventListener("touchstart", toggleMenu);
 
 const menuItems = document.querySelector("#menu").children;
 for (let index = 0; index < menuItems.length; index++) {
 	let item = menuItems[index];
-	item.addEventListener("click", toggleMenu);
+	item.addEventListener("click", toggleMenuItems);
 }
